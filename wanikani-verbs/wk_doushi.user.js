848d046bf8c6fc3f9a29b8c778f009e2242a434d
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2017-12-17 00:35:38 +0900

    added escaped versions for violentmonkey

--- wanikani-verbs/wk_doushi.user.js
+++ wanikani-verbs/wk_doushi.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        WaniKani Doushi Related Verbs
-// @version     0.6.0
+// @version     0.6.1
 // @author      acm
 // @description Adds a related verbs section to WaniKani
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
@@ -13,8 +13,8 @@
 // @updateURL   https://github.com/mwil/wanikani-userscripts/raw/master/wanikani-verbs/wk_doushi.user.js
 // @downloadURL https://github.com/mwil/wanikani-userscripts/raw/master/wanikani-verbs/wk_doushi.user.js
 //
-// @resource    doushi_db    https://raw.githubusercontent.com/mwil/wanikani-userscripts/master/wanikani-verbs/db/doushi.json
-// @resource    wk_verbs_db  https://raw.githubusercontent.com/mwil/wanikani-userscripts/master/wanikani-verbs/db/wk_verbs.json
+// @resource    doushi_db    https://raw.githubusercontent.com/mwil/wanikani-userscripts/master/wanikani-verbs/db/wk_doushi_esc.json
+// @resource    wk_verbs_db  https://raw.githubusercontent.com/mwil/wanikani-userscripts/master/wanikani-verbs/db/wk_verbs_esc.json
 //
 // @resource    doushi_style https://raw.githubusercontent.com/mwil/wanikani-userscripts/master/wanikani-verbs/css/doushi.css
 //
