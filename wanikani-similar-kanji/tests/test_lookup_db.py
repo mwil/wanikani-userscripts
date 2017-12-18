5b5f9267499301936db356a8930cd2beb27e39ce
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2017-12-18 11:40:16 +0900

    added tests, and fixed one undefined reading

---
+++ wanikani-similar-kanji/tests/test_lookup_db.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+import json
+
+LOOKUP_DB = "../db/lookup.json"
+
+
+def lookup_db_get_kanji_info():
+    with open(LOOKUP_DB, "r") as infile:
+        lookup_db = json.load(infile)
+
+    return lookup_db
+
+
+def test_entries_have_valid_readings():
+    for kanji, item in lookup_db_get_kanji_info().items():
+        assert len(item[item["important_reading"]]) > 0,\
+               "Kanji {} lacks a proper reading!".format(kanji)
