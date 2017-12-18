7281ac64e87ecd3a352afa800b15266146e44ec0
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2017-12-18 11:56:40 +0900

    Minified the escaped files, and fixed kanji not in jouyou

--- wanikani-phonetic-compounds/db/gen_esc.sh
+++ wanikani-phonetic-compounds/db/gen_esc.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-python -m json.tool kanji.json > kanji_esc.json
-python -m json.tool phonetic.json > phonetic_esc.json
+jq -ac . < kanji.json > kanji_esc.json
+jq -ac . < phonetic.json > phonetic_esc.json
