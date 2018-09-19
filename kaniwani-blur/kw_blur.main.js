b41f6f649bc11a5bfff8c22e1c116754d512ffbd
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2018-09-19 16:22:29 +0900

    Reformatting, small cosmetics

--- kaniwani-blur/kw_blur.main.js
+++ kaniwani-blur/kw_blur.main.js
@@ -8,19 +8,23 @@
 
     GM_addStyle(GM_getResourceText(`blur_style`));
 
+    // #########################################################################
     const app_callback = function(mutations)
     {
         console.log(mutations);
 
-        if (_.some(mutations, [`target.className`, `sc-kasBVs hzKNYV`]) ||
-            _.some(mutations, [`target.className`, `sc-laTMn eJYOxB`]))
+        if (_.some(mutations, [`target.className`, `sc-kasBVs hzKNYV`]) ||  /* Last change on transition */
+            _.some(mutations, [`target.className`, `sc-laTMn eJYOxB`]))     /* Last change on the initial (first) question */
         {
             $(`.kw_blurred`).remove();
 
             console.log("Specified mutation happened ...");
 
-            let $contain = $(`div.sc-dliRfk.bsdHG`)[0];  /* div containing a solution */
-            let solution = $($contain, `div.sc-iqzUVk.hzQWwO`).text(); /* this class contains exactly one character each */
+            /* first div containing a solution */
+            let $contain = $(`div.sc-dliRfk.bsdHG`)[0];
+
+            /* this class contains exactly one character each, join ... */
+            let solution = $($contain, `div.sc-iqzUVk.hzQWwO`).text();
 
             $(`<div>`)
                 .text(solution)
@@ -28,9 +32,10 @@
                 .appendTo(`h1`);
         }
     };
+    // #########################################################################
 
 
-    let app_obs = new MutationObserver(app_callback);
+    const app_obs = new MutationObserver(app_callback);
     app_obs.observe(document.getElementById(`app`), {childList: true, subtree: true});
 }
 )();
