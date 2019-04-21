9b9179075cded1c7e1b499478165d0a90d014fe0
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2019-04-21 17:24:35 +0900

    Added kw domain directly, and upped version number.

--- kaniwani-blur/kw_blur.user.js
+++ kaniwani-blur/kw_blur.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        KaniWani Blur
-// @version     0.3.0
+// @version     0.9.0
 // @author      acm
 // @description Adds an additional hint to reviews to make similar items easier to distinguish
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @namespace   kw_blur
 //
+// @include     *://kaniwani.com/*
 // @include     *://www.kaniwani.com/*
 //
 // @updateURL   https://github.com/mwil/wanikani-userscripts/raw/master/kaniwani-blur/kw_blur.user.js
