12787a9b9f4901f9fb828319ef40a947a14f0463
Pacharapol Withayasakpunt <polv@polv.cc>
2022-09-14 06:24:04 +0700

    Niai: custom Kanji styling

--- wanikani-similar-kanji/wk_niai.modal.js
+++ wanikani-similar-kanji/wk_niai.modal.js
@@ -34,7 +34,7 @@
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                         <h3 class="modal-title">
                             Settings &mdash; Niai Visually Similar Kanji for&nbsp;
-                            <form id="niai_head_kanji_form" style="display:inline-block" onsubmit="return false">
+                            <form id="niai_head_kanji_form" style="all:unset; display:inline-block" onsubmit="return false">
                                 <input id="niai_head_kanji_input" type="text" lang="ja" value="${
                                     kan
                                 }" style="all:unset; cursor:pointer">
