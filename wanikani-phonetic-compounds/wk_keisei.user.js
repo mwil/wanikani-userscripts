36b2bbcce5c972e4cfbe822921013584cb073beb
Sinyaven <86518751+Sinyaven@users.noreply.github.com>
2022-10-30 15:59:41 +0100

    Keisei now works with WaniKani's new history.pushState behavior for item pages

--- wanikani-phonetic-compounds/wk_keisei.user.js
+++ wanikani-phonetic-compounds/wk_keisei.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name        WaniKani Keisei Phonetic-Semantic Composition
-// @version     1.8.17
+// @version     1.8.18
 // @author      acm
 // @description Adds information to Wanikani about kanji that use Phonetic-Semantic Composition.
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @namespace   wk_keisei
 //
-// @include     *://www.wanikani.com/*
-// @include     *://preview.wanikani.com/*
+// @match       https://www.wanikani.com/*
+// @match       https://preview.wanikani.com/*
 //
 // @updateURL   https://github.com/mwil/wanikani-userscripts/raw/master/wanikani-phonetic-compounds/wk_keisei.user.js
 // @downloadURL https://github.com/mwil/wanikani-userscripts/raw/master/wanikani-phonetic-compounds/wk_keisei.user.js
@@ -24,7 +24,7 @@
 //
 // @require     https://cdn.jsdelivr.net/npm/lodash@4.17.4/lodash.min.js
 //
-// @require     https://greasyfork.org/scripts/430565-wanikani-item-info-injector/code/WaniKani%20Item%20Info%20Injector.user.js?version=1103761
+// @require     https://greasyfork.org/scripts/430565-wanikani-item-info-injector/code/WaniKani%20Item%20Info%20Injector.user.js?version=1111117
 //
 // @require     https://raw.githubusercontent.com/mwil/wanikani-userscripts/8ee517737d604f1df0ff103a33b69f1f07218815/wanikani-phonetic-compounds/wk_keisei.db.js
 // @require     https://raw.githubusercontent.com/mwil/wanikani-userscripts/8ee517737d604f1df0ff103a33b69f1f07218815/wanikani-phonetic-compounds/wk_keisei.strings.en.js
