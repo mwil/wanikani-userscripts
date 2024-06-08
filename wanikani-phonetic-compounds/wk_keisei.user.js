7772d144837ba41ef94d0d1a6b909b836dbbd093
lupomikti <25500382+lupomikti@users.noreply.github.com>
2024-06-08 16:52:30 -0700

    Niai and Keisei: update user.js versions

--- wanikani-phonetic-compounds/wk_keisei.user.js
+++ wanikani-phonetic-compounds/wk_keisei.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        WaniKani Keisei Phonetic-Semantic Composition
-// @version     1.8.38
+// @version     1.8.39
 // @author      acm
 // @description Adds information to Wanikani about kanji that use Phonetic-Semantic Composition.
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
